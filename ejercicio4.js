551ee2d709309b5860b7edc26d7b94ff61647f55
Ismael Perez <ismael.perezroldan@educa.madrid.org>
2023-12-13 11:40:56 +0100

    Add solution for exercise 4

---
+++ ejercicio4.js
@@ -0,0 +1,40 @@
+const op1 = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Operación 1 completada');
+    }, 1000);
+  });
+  
+  const op2 = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Operación 2 completada');
+    }, 2000);
+  });
+  
+Promise.all([op1, op2]).then((resultados) => {
+      console.log('Resultados:', resultados);
+})
+
+
+async function operacionesConcurrentes() {
+    try{
+        const resultados = [];
+        resultados.push(await op1);
+        resultados.push(await op2);
+        console.log(`resultados: ${resultados}`);
+    } catch(error){
+        console.error(`error: ${error}`)
+    }
+}
+
+operacionesConcurrentes()
+
+async function operacionesConcurrentes() {
+    try{
+        const resultados = Promise.all([op1, op2])
+        console.log(`resultados: ${resultados}`);
+    } catch(error){
+        console.error(`error: ${error}`)
+    }
+}
+
+operacionesConcurrentes()
